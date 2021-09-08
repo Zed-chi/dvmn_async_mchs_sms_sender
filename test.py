6eb4e14b478da82492b3592ed7066763c6572090
zed-chi <zed-chi@mail.ru>
2021-09-08 15:50:38 +0300

    changed requests to httpx

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 from environs import Env
 import requests
+import trio
+import httpx
 
 
 API_URL = "https://smsc.ru/sys/send.php"
@@ -10,10 +12,22 @@ password = env.str("PASSWORD")
 message = "Hello"
 test_phone = env.str("TESTPHONE")
 phones = [test_phone,]
-
-requests.post(API_URL, params={
+params = {
     "login":login,
     "psw":password,
     "phones":",".join(phones),
     "mes":message,
-})
+}
+
+def send_sms():
+    requests.post(API_URL, params=params)
+
+async def send_sms_async():
+    async with httpx.AsyncClient() as client:
+        res = await client.get("http://ya.ru")
+        res.raise_for_status()
+        print(res.status_code)
+        print(res.text)
+
+
+trio.run(send_sms_async)
