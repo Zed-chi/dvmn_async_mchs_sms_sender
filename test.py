2f16f6a436a3d582d301e078cc34f0797e97ab80
zed-chi <zed-chi@mail.ru>
2021-09-08 20:03:37 +0300

    added check status

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 from environs import Env
-import requests
+import json
 import trio
 import httpx
+import aiofiles
 
 
 API_URL = "https://smsc.ru/sys/send.php"
+STATUS_URL = "https://smsc.ru/sys/status.php"
 env = Env()
 env.read_env()
 login = env.str("LOGIN")
@@ -12,22 +14,48 @@ password = env.str("PASSWORD")
 message = "Hello"
 test_phone = env.str("TESTPHONE")
 phones = [test_phone,]
-params = {
-    "login":login,
-    "psw":password,
-    "phones":",".join(phones),
-    "mes":message,
-}
+test_id = 123
 
-def send_sms():
-    requests.post(API_URL, params=params)
 
 async def send_sms_async():
+    params = {
+        "login":login,
+        "psw":password,
+        "phones":",".join(phones),
+        "mes":message,
+        "fmt":3,
+        "id":test_id,
+        "op":1,
+        "all":1,
+    }
     async with httpx.AsyncClient() as client:
-        res = await client.get("http://ya.ru")
+        res = await client.post(API_URL, params=params)
         res.raise_for_status()
         print(res.status_code)
-        print(res.text)
+        print(res.json())
+        with open("out.txt", "a", encoding="utf-8") as file:
+            file.write(json.dumps(res.json()))
+            file.write("\n")
 
 
-trio.run(send_sms_async)
+async def check_sms_status(phone, sms_id):
+    params = {
+        "login":login,
+        "psw":password,
+        "phone":phone,
+        "id":sms_id,
+        "fmt":3,
+        "all":0,
+    }
+    async with httpx.AsyncClient() as client:
+        res = await client.post(STATUS_URL, params=params)
+        res.raise_for_status()
+        print(res.status_code)
+        print(res.json())
+
+    async with await trio.open_file("out.txt", "a", encoding="utf-8") as file:
+            await file.write(json.dumps(res.json()))
+            await file.write("\n")
+
+#trio.run(send_sms_async)
+trio.run(check_sms_status, test_phone, test_id)
