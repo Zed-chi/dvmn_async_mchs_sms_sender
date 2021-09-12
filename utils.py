885af678ac4c628900e516c2b2ea198569753b6d
zed-chi <zed-chi@mail.ru>
2021-09-12 23:57:13 +0300

    removed dataclass

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
+import argparse
 from dataclasses import dataclass
 from typing import Any
+
 from aioredis.pool import ConnectionsPool
 
 from db import Database
@@ -7,28 +9,38 @@ from db import Database
 
 @dataclass
 class State:
-    redis:ConnectionsPool = None
+    redis: ConnectionsPool = None
     db: Database = None
 
 
-@dataclass
-class Mailing:
-    timestamp: Any
-    SMSText: str
-    mailingId: str
-    totalSMSAmount: int
-    deliveredSMSAmount: int
-    failedSMSAmount: int
-
-
-def mail_conversion(mail_dict):    
-    failed = [v for k,v in mail_dict["phones"].items() if v == "failed"]
-    delivered = [v for k,v in mail_dict["phones"].items() if v == "delivered"]
+def mail_conversion(mail_dict):
+    failed = [v for k, v in mail_dict["phones"].items() if v == "failed"]
+    delivered = [v for k, v in mail_dict["phones"].items() if v == "delivered"]
     return {
-        "timestamp":mail_dict["created_at"],
+        "timestamp": mail_dict["created_at"],
         "SMSText": mail_dict["text"],
         "mailingId": str(mail_dict["sms_id"]),
         "totalSMSAmount": mail_dict["phones_count"],
         "deliveredSMSAmount": len(delivered),
         "failedSMSAmount": len(failed),
     }
+
+
+def create_argparser():
+    parser = argparse.ArgumentParser(
+        description="Redis database usage example"
+    )
+    parser.add_argument(
+        "--address",
+        default="redis://localhost:6379",
+        action="store",
+        dest="redis_uri",
+        help="Redis URI",
+    )
+    parser.add_argument(
+        "--password",
+        action="store",
+        dest="redis_password",
+        help="Redis db password",
+    )
+    return parser
